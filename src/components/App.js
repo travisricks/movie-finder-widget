b075ca17dcb2460d1dca0922f5c336893b020461
Travis Ricks <travis@travisricks.com>
2020-05-04 15:16:29 +0900

    create Search component structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Search from "./Search";
 import "../styles/App.css";
 
 const App = () => (
   <div className="App">
     <Header />
+    <Search />
   </div>
 );
 
---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default class Search extends React.Component {
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.elements.text.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="" onSubmit={this.handleFormSubmit}>
+          <input className="" type="text" name="text" />
+          <button className="button">Search</button>
+        </form>
+        <div>
+          <button>All</button>
+          <button>Movies</button>
+          <button>TV Shows</button>
+          <button>People</button>
+        </div>
+      </div>
+    );
+  }
+}
