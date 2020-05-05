288502ce91b21a3d0a922d21c7e529b097fc37bb
Travis Ricks <travis@travisricks.com>
2020-05-05 20:57:48 +0900

    style search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,21 +8,23 @@ export default class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        <form className="" onSubmit={this.handleFormSubmit}>
-          <input
-            className=""
-            type="text"
-            name="text"
-            placeholder="Search for movies, tv shows or people..."
-          />
-          <button className="button">Search</button>
+      <div className="search">
+        <form className="search-form" onSubmit={this.handleFormSubmit}>
+          <div className="input-wrapper">
+            <input
+              className="search-form__input"
+              type="text"
+              name="text"
+              placeholder="Search for movies, tv shows or people..."
+            />
+            <button className="search-form__button">Search</button>
+          </div>
         </form>
         <div>
-          <button>All</button>
-          <button>Movies</button>
-          <button>TV Shows</button>
-          <button>People</button>
+          <button className="filter-button filter-button-active">All</button>
+          <button className="filter-button">Movies</button>
+          <button className="filter-button">TV Shows</button>
+          <button className="filter-button">People</button>
         </div>
       </div>
     );
